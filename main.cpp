50bffa4a03c4fdfd1b2e0c1fd3e6e062f7052bf8
Gabriel Apodaca <gabrielapodaca4242@gmail.com>
2026-02-19 18:00:48 -0800

    Got file input working.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello world!\n";
+
+    std::string line;
+
+    std::ifstream my_file("test.txt");
+
+    if (my_file.is_open())
+    {
+        while (std::getline(my_file, line))
+        {
+            std::cout << line << '\n';
+        }
+        my_file.close();
+    }
+    else
+    {
+        std::cerr << "oiwejifojewf\n";
+    }
+
+
     return 0;
 }
