c3d980f70d250e3f045c4686ae0f43e4409c0d60
masonj5002 <masonj@csu.fullerton.edu>
2026-02-26 16:37:42 -0800

    simplified main structure

--- main.cpp
+++ main.cpp
@@ -4,34 +4,38 @@
 #include <utility>
 #include <string>
 
-void lex_scan(const std::string& line)
-{
-    /* */
-}
+// void lex_scan(const std::string& line)
+// {
+//     /* */
+// }
 
 
 
-std::pair<std::string, std::string> lexer(const std::string& token)
-{
+// std::pair<std::string, std::string> lexer(const std::string& token)
+// {
 
-    // 1. Pass it into the seperator FSM
+//     // 1. Pass it into the seperator FSM
 
-    // 2. Pass it into the keyword FSM
+//     // 2. Pass it into the keyword FSM
 
-    // 3. Pass it  into the operator FSM
+//     // 3. Pass it  into the operator FSM
 
-    // 4. Pass it into the identifier FSM
+//     // 4. Pass it into the identifier FSM
 
-    // 5. Pass it into the real FSM
+//     // 5. Pass it into the real FSM
 
-    // 6. Pass it into the integer FSM
+//     // 6. Pass it into the integer FSM
 
-    return {};
-}
+//     return {};
+// }
+
+// TODO: Make a class for the cursor
+// TODO: Make a class for each FSM (or all of them in the same class)
 
 int main() {
 
-    std::vector<std::pair<int, int>> test = {{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}};
+    // TODO: Finish state table
+    // TODO: Move state table to separate class file
     std::vector<std::pair<std::string, std::vector<int>>> state_table = 
     {
         { "l", {2, 2, 1}},
@@ -48,60 +52,33 @@ int main() {
         { "p", {0}},        // punctuation
     };
 
-    // std::map<std::string, std::vector<int>> state_table =
-    // {
-    //   { , }  
-    // };
-
-    // for (const auto& p : state_table) std::cout << p.first
-
-    // while (std::cin >> variable)
-    // std::vector<std::vector<int>> state_table;
-
-    // for (const auto& p : state_table)
-    // {
-    //     std::cout << '{' << p.first << ',' << p.second << "}\n";
-    // }
-
+    
     std::vector<char>        separators = { '@', '(', ')', ',', '{', '}', ';'};
     std::vector<std::string> keywords   = { "function", "integer", "boolean", "real", "if", "fi", "otherwise", "return", "write", "read", "while", "true", "false"};
     std::vector<std::string> operators  = {"=", "+", "-", "*", "/", "==", "!=", ">", "<", "<=", "=>"};
     
-    std::string line;
-
-    std::ifstream my_file("test.txt");
-
-    if (!my_file.is_open())
+    
+    const std::string SOURCE_FILE = "source_code.txt";
+    std::ifstream my_file(SOURCE_FILE);
+    
+    if (!my_file)
     {
         std::cerr << "Error opening test.txt\n";
-        return -1;
+        return 1;
+    }
+    
+    // Read the file
+    std::string line;
+    while (std::getline(my_file, line)) {
+        std::cout << line << "\n";
     }
 
-    // prints text file
-    while (!my_file.eof())
-    {
-        std::getline(my_file, line, '\n');
-        // std::cout << line << '\n';
-        // for each character on each line, determine what the character is
-        for (char c : line) {
-            // std::cout << c;
-            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-                //std::cout << "this is a letter\n";
-                std::cout << state_table[0].second[0] << std::endl;
-            }
-        }
-        std::cout << "\n";
-    }
     my_file.close();
 
-    // if (my_file.is_open())
-    // {
-    //     while ()
-    //     {
-    //         std::cout << line << '\n';
-    //     }
-    //     my_file.close();
-    // }
+    // OUTPUT
+
+    // TODO: Output needs to be saved to an output text file. Each type of token should
+    // be labeled but the prof is flexible on how the file is output
 
     return 0;
 }
