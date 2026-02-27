31725d726ec65867240d1e3695bf70cf9bbf4bb1
masonj5002 <masonj@csu.fullerton.edu>
2026-02-26 16:38:42 -0800

    format `main()`

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
-#include <iostream>
 #include <fstream>
-#include <vector>
-#include <utility>
+#include <iostream>
 #include <string>
+#include <utility>
+#include <vector>
 
 // void lex_scan(const std::string& line)
 // {
 //     /* */
 // }
-
-
 
 // std::pair<std::string, std::string> lexer(const std::string& token)
 // {
@@ -33,40 +31,30 @@
 // TODO: Make a class for each FSM (or all of them in the same class)
 
 int main() {
-
     // TODO: Finish state table
     // TODO: Move state table to separate class file
-    std::vector<std::pair<std::string, std::vector<int>>> state_table = 
-    {
-        { "l", {2, 2, 1}},
-        { "d", {1}},
-        { "{", {2}},
-        { "}", {0}},
-        { "(", {0}},
-        { "*", {0}},
-        { ")", {0}},
-        { ":", {0}},
-        { "=", {0}},
-        { "<", {0}},
-        { "sp", {1, 3, 1}}, // space
-        { "p", {0}},        // punctuation
+    std::vector<std::pair<std::string, std::vector<int>>> state_table = {
+        {"l", {2, 2, 1}}, {"d", {1}}, {"{", {2}},        {"}", {0}},
+        {"(", {0}},       {"*", {0}}, {")", {0}},        {":", {0}},
+        {"=", {0}},       {"<", {0}}, {"sp", {1, 3, 1}},  // space
+        {"p", {0}},                                       // punctuation
     };
 
-    
-    std::vector<char>        separators = { '@', '(', ')', ',', '{', '}', ';'};
-    std::vector<std::string> keywords   = { "function", "integer", "boolean", "real", "if", "fi", "otherwise", "return", "write", "read", "while", "true", "false"};
-    std::vector<std::string> operators  = {"=", "+", "-", "*", "/", "==", "!=", ">", "<", "<=", "=>"};
-    
-    
+    std::vector<char> separators = {'@', '(', ')', ',', '{', '}', ';'};
+    std::vector<std::string> keywords = {
+        "function", "integer", "boolean", "real",  "if",   "fi",   "otherwise",
+        "return",   "write",   "read",    "while", "true", "false"};
+    std::vector<std::string> operators = {
+        "=", "+", "-", "*", "/", "==", "!=", ">", "<", "<=", "=>"};
+
     const std::string SOURCE_FILE = "source_code.txt";
     std::ifstream my_file(SOURCE_FILE);
-    
-    if (!my_file)
-    {
+
+    if (!my_file) {
         std::cerr << "Error opening test.txt\n";
         return 1;
     }
-    
+
     // Read the file
     std::string line;
     while (std::getline(my_file, line)) {
@@ -77,8 +65,8 @@ int main() {
 
     // OUTPUT
 
-    // TODO: Output needs to be saved to an output text file. Each type of token should
-    // be labeled but the prof is flexible on how the file is output
+    // TODO: Output needs to be saved to an output text file. Each type of token
+    // should be labeled but the prof is flexible on how the file is output
 
     return 0;
 }
