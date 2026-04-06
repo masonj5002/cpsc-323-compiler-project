0dd533abe6f731475183b3a0d95effa9d191cf27
Gabriel Apodaca <gabrielapodaca4242@gmail.com>
2026-04-05 19:56:09 -0700

    syntax errors printed to console output now

--- prdp.hpp
+++ prdp.hpp
@@ -102,15 +102,21 @@ class Rat26SParser
         m_current_production = production;
     }
     
-    // Writes to the output file a formatted syntax error message that includes the token, lexeme,
+    // Writes to the output file and console output a formatted syntax error message that includes the token, lexeme,
     // expected message, and the current production that was violated.
     void output_error(const std::string& expected_message)
     {
-        m_output_file_stream << "\nSyntax Error in " << m_input_file_name << " on line " << get_current_record().line << " with\n"
+        m_output_file_stream << "\nRat26S Syntax Error in " << m_input_file_name << " on line " << get_current_record().line << " with\n"
                              << "\tToken       : " << get_current_record().token  << '\n'
                              << "\tLexeme      : " << get_current_record().lexeme << '\n'
                              << "\tExpected    : " << expected_message            << '\n'
                              << "\tViolation of: " << m_current_production        << '\n';
+        
+        std::cout << "\nRat26S Syntax Error in " << m_input_file_name << " on line " << get_current_record().line << " with\n"
+                  << "\tToken       : " << get_current_record().token  << '\n'
+                  << "\tLexeme      : " << get_current_record().lexeme << '\n'
+                  << "\tExpected    : " << expected_message            << '\n'
+                  << "\tViolation of: " << m_current_production        << '\n';
     }
 
     // Helper function to check if current token is in a set
